2f3de422290419bcda0e4d87254a9f377b09455e
JaTin <jigyashusaini7@gmail.com>
2024-09-16 12:23:29 +0530

    adding landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
 
 
 const LandingPage = () => {
-  return (
-    <div>
-      This is landing page
-    </div>
-  );
+
+
+    const notifications = [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet fugiat nam officia qui quidem, quos reprehenderit sint sit veritatis voluptatem.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dolorum iste iusto labore laudantium nulla optio reiciendis totam vel?",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eos ex facilis in libero nam quaerat quibusdam quo tenetur veritatis!",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut cumque eius ipsa ipsam laboriosam nulla odio, quam repudiandae voluptatibus!",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit illo magni molestiae non obcaecati omnis qui temporibus tenetur vitae voluptate.", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet fugiat nam officia qui quidem, quos reprehenderit sint sit veritatis voluptatem.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dolorum iste iusto labore laudantium nulla optio reiciendis totam vel?",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eos ex facilis in libero nam quaerat quibusdam quo tenetur veritatis!",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut cumque eius ipsa ipsam laboriosam nulla odio, quam repudiandae voluptatibus!",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit illo magni molestiae non obcaecati omnis qui temporibus tenetur vitae voluptate.",
+    ]
+
+
+    return (
+        <div className="flex justify-center align-middle items-center">
+            <div className="flex max-w-[1400px] flex-clo w-full flex-wrap justify-evenly m-4">
+                <img src="https://placehold.co/600x300"/>
+                <div className="p-4">
+                    <h2 className="mb-2 text-xlg underline font-semibold text-gray-900">Notifications:</h2>
+                    <div>
+                        <ul className="max-w-md space-y-1 text-text text-sm list-disc list-inside dark:text-gray-400 max-h-[300px] overflow-y-scroll">
+                            {notifications.map((n) => <li>{n}</li>)}
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LandingPage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,17 @@ export default {
     ],
     theme: {
         extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'blue-main': '#004AAD', // Deep Blue (Primary Color)
+            'blue-secondary': '#1E90FF', // Sky Blue (Secondary Color)
+            'gold-accent': '#FFD700', // Gold (Accent Color)
+            'background': '#F5F5F5', // Light Gray (Background Color)
+            'text': '#333333', // Dark Gray (Text Color)
+        },
+
     },
     plugins: [
         require('daisyui'),
